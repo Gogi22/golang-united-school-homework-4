645b62276a87fda9a44dcbd54f40335ed323f93c
Gogi <Gogi22@users.noreply.github.com>
2022-05-14 11:49:19 +0400

    added errors

--- string_sum.go
+++ string_sum.go
@@ -7,30 +7,19 @@ import (
 	"strings"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
-	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
-	// Use when the expression has number of operands not equal to two
-	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	errorEmptyInput            = errors.New("input is empty")
+	errorNotTwoOperands        = errors.New("expecting two operands, but received more or less")
+	errorUnsupportedCharacters = errors.New("some characters are not supported")
+	errorInvalidInput          = errors.New("input is not valid")
 )
-
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
 	arr := make([]rune, 0)
 	operands := 0
 	for _, v := range input {
 		if !strings.Contains("0123456789+-", string(v)) {
-			return "", fmt.Errorf("use of unsupported character")
+			return "", fmt.Errorf("something went wrong: %w", errorUnsupportedCharacters)
 		}
 		if v != ' ' {
 			arr = append(arr, v)
@@ -44,14 +33,18 @@ func StringSum(input string) (output string, err error) {
 		return "", fmt.Errorf("something went wrong: %w", errorEmptyInput)
 	}
 
-	if !(len(arr) == 4 && operands == 2 || len(arr) == 3 && operands == 1) {
+	if operands != 2 {
 		return "", fmt.Errorf("something went wrong: %w", errorNotTwoOperands)
+	}
+
+	if !(len(arr) == 4 && operands == 2 || len(arr) == 3 && operands == 1) {
+		return "", fmt.Errorf("something went wrong: %w", errorInvalidInput)
 	}
 
 	second, ok := strconv.ParseInt(string(arr[len(arr)-1]), 10, 0)
 
 	if ok != nil {
-		return "", fmt.Errorf("invalid input")
+		return "", fmt.Errorf("something went wrong: %w", errorInvalidInput)
 	}
 
 	if arr[len(arr)-2] == '-' {
@@ -61,7 +54,7 @@ func StringSum(input string) (output string, err error) {
 	first, ok := strconv.ParseInt(string(arr[len(arr)-3]), 10, 0)
 
 	if ok != nil {
-		return "", fmt.Errorf("invalid input")
+		return "", fmt.Errorf("something went wrong: %w", errorInvalidInput)
 	}
 
 	if len(arr) == 4 && arr[0] == '-' {
